11876fc077fb3e876e196eeb821c124d55c43ad8
Martín <blackhack1@hotmail.com>
2022-04-02 15:59:09 -0300

    added printFromTask

--- print.sh
+++ print.sh
@@ -1,9 +1,9 @@
 echo '================================================='
 echo 'Printing output through bash script'
 #actual command to perform
-java -jar build/libs/GradleProject-1.0-SNAPSHOT.jar > log.txt
+java -jar build/libs/GradleProject-1.0-SNAPSHOT.jar > logFromScript.txt
 echo '-------------------------------------------------'
-echo 'java -jar build/libs/GradleProject-1.0-SNAPSHOT.jar > log.txt'
+echo 'java -jar build/libs/GradleProject-1.0-SNAPSHOT.jar > logFromScript.txt'
 echo '-------------------------------------------------'
 echo 'Just printed main output in log.txt file'
 echo '================================================='
