8d236f26518c6493e0910566eed24f0f0ff9073b
Martín <blackhack1@hotmail.com>
2022-04-03 19:35:14 -0300

    changes

--- print.sh
+++ print.sh
@@ -1,9 +1,9 @@
 echo '================================================='
-echo 'Printing output through bash script'
+echo 'Printing output from bash script'
 #actual command to perform
 java -jar build/libs/GradleProject-1.0-SNAPSHOT.jar > logFromScript.txt
 echo '-------------------------------------------------'
 echo 'java -jar build/libs/GradleProject-1.0-SNAPSHOT.jar > logFromScript.txt'
 echo '-------------------------------------------------'
-echo 'Just printed main output in log.txt file'
+echo 'Main output in logFromScript.txt file'
 echo '================================================='
